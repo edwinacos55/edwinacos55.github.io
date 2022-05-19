b9a5890296d40bdbc23b240b8e9460305335c4fa
edwinacos55 <edwinacos55@gmail.com>
2022-05-18 22:35:39 -0400

    modified App.js to say Edwiin Acosta

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Edwin Acosta and I'm going to master React!
+          My name is Edwin Acosta and I'm going to master React!!
         </p>
         <a
           className="App-link"
